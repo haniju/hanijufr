cccc94a1f7cd750b1cd69a567c1011b993128226
haniju <hannequin.j@gmail.com>
2018-05-06 23:53:49 +0200

    meh

--- adf-story/js/dragging.js
+++ adf-story/js/dragging.js
@@ -1,4 +1,50 @@
-  document.getElementById("dragOeuil").onmousedown = function(e) {
+//autre piste https://stackoverflow.com/questions/14142677/how-to-use-getelementsbyclassname-in-javascript-function
+function dragDiv() {
+var dragged = document.getElementsByClassName('draggable'),
+i = dragged.lenght;
+while(i--) {
+  dragged[i].onmousedown = function(e1);
+  dragged[i].onmousemove = function(e2);
+  dragged[i].onmouseup = function(e3);
+}
+function(e1) {
+this.prevX = e.clientX;
+this.prevY = e.clientY;
+this.mouseDown = true;
+}
+function(e2) {
+if(this.mouseDown) {
+this.style.left = (Number(this.style.left.substring(0, this.style.left.length-2)) + (e.clientX - this.prevX)) + "px";
+this.style.top = (Number(this.style.top.substring(0, this.style.top.length-2)) + (e.clientY - this.prevY)) + "px";
+}
+this.prevX = e.clientX;
+this.prevY = e.clientY;
+}
+function(e3) {
+this.mouseDown = false;
+}
+)}
+}
+
+//https://jsfiddle.net/6gq7u8Lc/
+/*
+    document.getElementById("dragOeuil").onmousedown = function(e) {
+      this.prevX = e.clientX;
+      this.prevY = e.clientY;
+      this.mouseDown = true;
+    }
+    document.getElementById("dragOeuil").onmousemove = function(e) {
+      if(this.mouseDown) {
+        this.style.left = (Number(this.style.left.substring(0, this.style.left.length-2)) + (e.clientX - this.prevX)) + "px";
+        this.style.top = (Number(this.style.top.substring(0, this.style.top.length-2)) + (e.clientY - this.prevY)) + "px";
+      }
+      this.prevX = e.clientX;
+      this.prevY = e.clientY;
+    }
+    document.getElementById("dragOeuil").onmouseup = function(e) {
+      this.mouseDown = false;
+    }
+    document.getElementById("dragOeuil").onmousedown = function(e) {
   		this.prevX = e.clientX;
   		this.prevY = e.clientY;
   		this.mouseDown = true;
@@ -11,10 +57,11 @@
   		this.prevX = e.clientX;
   		this.prevY = e.clientY;
   	}
-  	document.getElementById("dragOeuil").onmouseup = function() {
+  	document.getElementById("dragOeuil").onmouseup = function(e) {
   		this.mouseDown = false;
   	}
-  document.getElementById("dragNez").onmousedown = function(e) {
+
+    document.getElementById("dragNez").onmousedown = function(e) {
   		this.prevX = e.clientX;
   		this.prevY = e.clientY;
   		this.mouseDown = true;
@@ -30,7 +77,8 @@
   	document.getElementById("dragNez").onmouseup = function() {
   		this.mouseDown = false;
   	}
-  document.getElementById("dragBou").onmousedown = function(e) {
+
+    document.getElementById("dragBou").onmousedown = function(e) {
   		this.prevX = e.clientX;
   		this.prevY = e.clientY;
   		this.mouseDown = true;
@@ -46,7 +94,8 @@
   	document.getElementById("dragBou").onmouseup = function() {
   		this.mouseDown = false;
   	}
-  document.getElementById("dragProt").onmousedown = function(e) {
+
+    document.getElementById("dragProt").onmousedown = function(e) {
   		this.prevX = e.clientX;
   		this.prevY = e.clientY;
   		this.mouseDown = true;
@@ -62,3 +111,4 @@
   	document.getElementById("dragProt").onmouseup = function() {
   		this.mouseDown = false;
   	}
+*/
