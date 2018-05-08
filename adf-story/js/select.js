e7b46066d367bfccbef6b0d20b9a78e42b5daf2d
haniju <hannequin.j@gmail.com>
2018-05-08 13:47:29 +0200

    Stable draggable with hover highlights

--- adf-story/js/select.js
+++ adf-story/js/select.js
@@ -4,21 +4,35 @@ function setTex() {
     img.src = this.value;
     return false;
 }
+function showDivT() {
+  document.getElementById("texture").style.visibility = "visible";
+}
 document.getElementById("dropTex").onchange = setTex;
 
 
-function setOeuil() {
-    var img = document.getElementById("oeuil");
-    img.src = this.value;
-    return false;
-}
+//document.getElementById("dropOeuil").addEventListener("change", setOeuil);
+
+  function setOeuil() {
+      var img = document.getElementById("oeuil");
+      img.src = this.value;
+      return false;
+  };
+  function showDivO() {
+    document.getElementById("dragOeuil").style.visibility = "visible";
+  };
+//document.getElementById("dropOeuil").onchange = showDivO;
 document.getElementById("dropOeuil").onchange = setOeuil;
+
 
 function setNez() {
     var img = document.getElementById("nez");
     img.src = this.value;
     return false;
 }
+function showDivN() {
+  document.getElementById("dragNez").style.visibility = "visible";
+}
+//document.getElementById("dropNez").onchange = showDivN;
 document.getElementById("dropNez").onchange = setNez;
 
 function setBou() {
@@ -26,11 +40,17 @@ function setBou() {
     img.src = this.value;
     return false;
 }
+function showDivB() {
+  document.getElementById("dragBou").style.visibility = "visible";
+}
 document.getElementById("dropBou").onchange = setBou;
 
 function setProt() {
     var img = document.getElementById("prot");
     img.src = this.value;
     return false;
+}
+function showDivP() {
+  document.getElementById("dragProt").style.visibility = "visible";
 }
 document.getElementById("dropProt").onchange = setProt;
